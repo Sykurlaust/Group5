c48266859e527cc3e5dddc092010365b8b7f61fb
LXLLXVX <lalave41@gmail.com>
2026-03-03 10:32:45 +0000

    feat: login-path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
+import Login from './pages/loginpage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
