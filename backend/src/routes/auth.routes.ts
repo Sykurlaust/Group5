ee9f54cc1f5d3cc9b6d6033f15783be3d2395f40
LXLLXVX <lalave41@gmail.com>
2026-03-10 09:24:39 +0000

    routes-problem

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express"
+import { authenticate } from "../middlewares/authenticate.js"
+import { register, getMe, updateMe } from "../controllers/auth.controller.js"
+
+const router = Router()
+
+router.post("/register", authenticate, register)
+router.get("/me", authenticate, getMe)
+router.put("/me", authenticate, updateMe)
+
+export default router
